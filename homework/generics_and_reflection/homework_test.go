8df3eec6415b4f03373397836b0faa43bfa00f29
DanielMoroz97 <72060511+DanielMoroz97@users.noreply.github.com>
2025-06-07 17:05:30 +0300

    homework_9

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,51 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	var pairs []string
+	var stringResult string
+
+	v := reflect.ValueOf(person)
+	t := reflect.TypeOf(person)
+	numField := t.NumField()
+
+	for i := 0; i < numField; i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+
+		tag := field.Tag.Get("properties")
+		if tag == "" {
+			continue
+		}
+		parts := strings.Split(tag, ",")
+		key := parts[0]
+		isOmitempty := len(parts) > 1 && parts[1] == "omitempty"
+		if isOmitempty && value.IsZero() {
+			continue
+		}
+
+		switch value.Kind() {
+		case reflect.String:
+			stringResult = value.String()
+		case reflect.Int:
+			stringResult = fmt.Sprintf("%d", value.Int())
+		case reflect.Bool:
+			stringResult = fmt.Sprintf("%t", value.Bool())
+		default:
+			continue
+		}
+		stringResult = castValue(stringResult)
+		pairs = append(pairs, fmt.Sprintf("%s=%s", key, stringResult))
+	}
+
+	return strings.Join(pairs, "\n")
+}
+
+func castValue(value string) string {
+	value = strings.ReplaceAll(value, `\`, `\\`)
+	value = strings.ReplaceAll(value, "=", `\=`)
+	value = strings.ReplaceAll(value, ":", `\:`)
+	value = strings.ReplaceAll(value, "\n", `\n`)
+	return value
 }
 
 func TestSerialization(t *testing.T) {
