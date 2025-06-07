5fc0d41610443b371f83ac1384f12ecc462c7aae
DanielMoroz97 <72060511+DanielMoroz97@users.noreply.github.com>
2025-06-07 16:43:03 +0300

    homework_8

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +12,55 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errs []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	if len(e.errs) == 0 {
+		return ""
+	}
+	var result strings.Builder
+	count := len(e.errs)
+	if count == 1 {
+		return e.errs[0].Error()
+	}
+	fmt.Fprintf(&result, "%d errors occured:\n", count)
+	result.WriteString("\t* ")
+	result.WriteString(e.errs[0].Error())
+	for _, err := range e.errs[1:] {
+		result.WriteString("\t* ")
+		result.WriteString(err.Error())
+	}
+	result.WriteString("\n")
+	return result.String()
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	var result []error
+
+	if err != nil {
+		if multi, ok := err.(*MultiError); ok {
+			result = append(result, multi.errs...)
+		} else {
+			result = append(result, err)
+		}
+	}
+
+	for _, err := range errs {
+		if err != nil {
+			if multi, ok := err.(*MultiError); ok {
+				result = append(result, multi.errs...)
+			} else {
+				result = append(result, err)
+			}
+		}
+	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	return &MultiError{errs: result}
 }
 
 func TestMultiError(t *testing.T) {
